c07a88f1f61fcf7624e07263b011e9a154cb1b71
charithmadhuranga <nikhuge@gmail.com>
2022-08-10 15:13:22 +0530

    vuejs fullstack

---
+++ server/routes/api/posts.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const mongodb = require('mongodb');
+
+const router = express.Router();
+
+
+const uri = "mongodb+srv://charith:Skyline1234%40A@cluster0.z71my7s.mongodb.net/?retryWrites=true&w=majority";
+
+
+
+// Get Posts
+router.get('/',async (req,res) =>{
+    const posts = await loadPostCollection();
+    res.send(await posts.find({}).toArray());
+});
+
+// Add Posts
+router.post('/',async (req,res) =>{
+    const posts = await loadPostCollection();
+    await posts.insertOne({
+        text: req.body.text,
+        createdAt: new Date()
+    });
+    res.status(201).send();
+});
+
+// Delete Posts
+router.delete('/:id',async (req,res) =>{
+
+    const posts = await loadPostCollection();
+    await posts.deleteOne(
+        {
+            _id: new mongodb.ObjectId(req.params.id)
+        });
+    res.status(200).send();
+});
+
+async function loadPostCollection(){
+    const client = await mongodb.MongoClient.connect(
+        uri,
+        {
+            useNewUrlParser: true
+        }
+    );
+    return client.db('vue_express').collection('posts');
+}
+
+module.exports = router;
